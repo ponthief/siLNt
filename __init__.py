636923aa11a2f20252741881ea5c13b449e17d32
Ponthief <ponthief@bitbest.net>
2026-04-10 00:27:40 +0100

    no background jobs

--- __init__.py
+++ __init__.py
@@ -14,21 +14,8 @@ silnt_static_files = [
     }
 ]
 
-siLNt_ext: APIRouter = APIRouter(prefix="/silnt", tags=["silnt"])
+siLNt_ext: APIRouter = APIRouter(prefix="/siLNt", tags=["siLNt"])
 siLNt_ext.include_router(silnt_generic_router)
 siLNt_ext.include_router(silnt_api_router)
 
-scheduled_tasks: list[asyncio.Task] = []
-
-def silnt_start():
-    from lnbits.tasks import create_permanent_unique_task    
-    task = create_permanent_unique_task("ext_silnt", wait_for_paid_invoices)
-    scheduled_tasks.append(task)
-       
-def silnt_stop():    
-    for task in scheduled_tasks:
-        try:
-            task.cancel()
-        except Exception as ex:
-            logger.warning(ex)
-__all__ = ["silnt_start", "silnt_stop", "siLNt_ext", "silnt_static_files", "db"]
+__all__ = ["siLNt_ext", "silnt_static_files", "db"]
